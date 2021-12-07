e7fb514e35675f81c7eefafc323debde131e5a1e
MarcioACOJ <marcio.alexandre.correa.oliveira@gmail.com>
2021-12-07 11:21:07 -0300

    user login part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(email: "admin@admin.com", password: "123456")
+Admin.create!(email: "admin@admin.com", password: "123456", name: "adminmaster")
 puts "admin created"
